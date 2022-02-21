bf6e806d8396b83a2bc4d9b3508ba7b1fb312ad5
cristianllambro <llambro.cristian1996@gmail.comm>
2022-02-21 15:24:17 +0100

    calc done

--- script.js
+++ script.js
@@ -6,5 +6,6 @@ let etaUser = prompt('eta passeggero?')
 if (etaUser <= 17) {
     let meterEuro = meterUser * 0.21;
     let discount = (meterEuro * 20) / 100;
-    document.getElementById('travel').innerHTML = discount
+    totEuro = meterEuro - discount;
+    document.getElementById('travel').innerHTML = totEuro
 }
