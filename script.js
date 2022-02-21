483cdc29fc46fdc280983bbc0de41f120f046bfb
cristianllambro <llambro.cristian1996@gmail.comm>
2022-02-21 15:26:12 +0100

    double number add

--- script.js
+++ script.js
@@ -7,5 +7,6 @@ if (etaUser <= 17) {
     let meterEuro = meterUser * 0.21;
     let discount = (meterEuro * 20) / 100;
     totEuro = meterEuro - discount;
+    totEuro = totEuro.toFixed(2)
     document.getElementById('travel').innerHTML = totEuro
 }
