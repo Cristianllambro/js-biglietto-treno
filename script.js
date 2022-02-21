beb382723a121cd2fec7c8c6b508052ea3bf0c02
cristianllambro <llambro.cristian1996@gmail.comm>
2022-02-21 15:09:17 +0100

    second try

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let meterUser = prompt('chilometri percorsi?')
 let etaUser = prompt('eta passeggero?')
-let output = document.getElementById('travel')
+
 let meterEuro = meterUser * 0.21;
 let discount = meterEuro * 20 / 100;
 
+
 if (etaUser <= 17) {
-    
+    discount = document.getElementById('travel')
 }
