d546d15f53980e475add632bfd25c03903fa394a
cristianllambro <llambro.cristian1996@gmail.comm>
2022-02-21 15:06:10 +0100

    try calc

---
+++ script.js
@@ -0,0 +1,9 @@
+let meterUser = prompt('chilometri percorsi?')
+let etaUser = prompt('eta passeggero?')
+let output = document.getElementById('travel')
+let meterEuro = meterUser * 0.21;
+let discount = meterEuro * 20 / 100;
+
+if (etaUser <= 17) {
+    
+}
